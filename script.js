fc0fd5a8bd440f064b88de38de93ee6ae2e1c89c
Aravind Annadata <115813455+aravind-415@users.noreply.github.com>
2024-05-16 13:09:32 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,33 @@
+
+ScrollReveal({
+  reset:true,
+  distance: '40px',
+  duration: 1000,
+});
+ScrollReveal().reveal('.image',{delay: 400, origin: 'bottom'});
+ScrollReveal().reveal('.metacomic',{delay: 200, origin: 'bottom'});
+ScrollReveal().reveal('.header p',{delay: 600, origin: 'bottom'});
+ScrollReveal().reveal('.card p',{delay: 400, origin: 'bottom'});
+ScrollReveal().reveal('.services h1',{delay: 400, origin: 'bottom'});
+ScrollReveal().reveal('.services .cards',{delay: 200, origin: 'bottom'});
+ScrollReveal().reveal('.services h4',{delay: 200, origin: 'bottom'});
+ScrollReveal().reveal('.services .but',{delay: 500, origin: 'top'});
+ScrollReveal().reveal('.description h2',{delay: 200, origin: 'top'});
+ScrollReveal().reveal('.description img',{delay: 400, origin: 'left'});
+ScrollReveal().reveal('.description p',{delay: 400, origin: 'right'});
+ScrollReveal().reveal('.contactus h1',{delay: 200, origin: 'top'});
+ScrollReveal().reveal('.location h1',{delay: 400, origin: 'top'});
+ScrollReveal().reveal('.location iframe',{delay: 400, origin: 'bottom'});
+ScrollReveal().reveal('.contactus .form',{delay: 400, origin: 'left'});
+ScrollReveal().reveal('.contactus .img',{delay: 400, origin: 'right'});
+ScrollReveal().reveal('.footer-cta',{delay: 400, origin: 'bottom'});
+ScrollReveal().reveal('.footer-logo',{delay: 200, origin: 'top'});
+ScrollReveal().reveal('.footer-widget-heading',{delay: 300, origin: 'top'});
+ScrollReveal().reveal('.footer-widget ul',{delay: 500, origin: 'bottom'});
+ScrollReveal().reveal('.footer-text',{delay: 500, origin: 'bottom'});
+ScrollReveal().reveal('.footer-social-icon',{delay: 500, origin: 'bottom'});
+ScrollReveal().reveal('.copy p',{delay: 500, origin: 'top'});
+window.addEventListener('DOMContentLoaded',function(){
+  this.window.scrollTo(0,0);
+});
+
